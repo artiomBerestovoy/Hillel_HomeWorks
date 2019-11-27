61102282cf6e02b251b81bb91895cb840d87e458
artiomBerestovoy <berstvjj@gmail.com>
2019-11-27 21:41:44 +0200

    add new file

---
+++ HW1/ArrayExample.java
@@ -0,0 +1,22 @@
+/*
+ *	This is simple Java program.
+ *	Call this file "ArrayExample.java"
+ *	Version 25.11.2019
+ *	Author Berestovoy Artiom
+ */ 
+ public class ArrayExample{
+	 public static void main(String[] args){
+		int[] exampArray = {4, 5, 12, 68, 8, 3, 71, 3};
+		
+		System.out.println();
+		
+		for(int i: exampArray){
+			System.out.println(i + "\n");
+		}
+		System.out.println();
+		
+		for(int i: exampArray){
+			System.out.print(i + ", ");
+		}
+	 }
+ }
