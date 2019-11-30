a9ab2b722a1406eadf7973704beeed13c5dbdd0d
artiomBerestovoy <berstvjj@gmail.com>
2019-11-30 13:49:00 +0200

    modified file

--- HW2/src/PrimeNumbers.java
+++ HW2/src/PrimeNumbers.java
@@ -1,24 +1,24 @@
 public class PrimeNumbers {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		/* Define and initialize the array */
-		int[] numbArray = { 5, 48, 3, 33, 7, 67, 98, 17, 46, };
+        /* Define and initialize the array */
+        int[] numbArray = {5, 48, 3, 33, 7, 67, 98, 17, 46,};
 
-		/* Iterate over the array */
-		for (int i : numbArray) {
-			int count = 0;
+        /* Iterate over the array */
+        for (int i : numbArray) {
+            int count = 0;
 
-			for (int k = 2; k < 100; k++) {
-				if ((i % k) == 0) {
-					count++;
-				}
-			}
+            for (int k = 2; k < 100; k++) {
+                if ((i % k) == 0) {
+                    count++;
+                }
+            }
 
-			if (count == 1) {
-				System.out.println(i);
-			}
-		}
-	}
+            if (count == 1) {
+                System.out.println(i);
+            }
+        }
+    }
 
 }
