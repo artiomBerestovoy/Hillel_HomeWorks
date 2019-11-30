9f6732726f5aa48773fd30af45975c074a4d0345
artiomBerestovoy <berstvjj@gmail.com>
2019-11-30 13:57:11 +0200

    modified file

--- HW2/src/EvenAndOddNumbers.java
+++ HW2/src/EvenAndOddNumbers.java
@@ -1,19 +1,19 @@
 public class EvenAndOddNumbers {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		/* Define and initialize the array */
-		int[] numbArray = { 5, 48, 3, 33, 7, 67, 98, 17, 46, };
+        /* Define and initialize the array */
+        int[] numbArray = { 5, 48, 3, 33, 7, 67, 98, 17, 46, };
 
-		/* Iterate over the array */
-		for (int i : numbArray) {
+        /* Iterate over the array */
+        for (int i : numbArray) {
 
-			if ((i % 2) == 0) {
-				System.out.println("The number " + i + " is even");
-			} else {
-				System.out.println("The number " + i + " is odd");
-			}
-		}
-	}
+            if ((i % 2) == 0) {
+                System.out.println("The number " + i + " is even");
+            } else {
+                System.out.println("The number " + i + " is odd");
+            }
+        }
+    }
 
 }
--- HW2/src/LargestAndSmallest.java
+++ HW2/src/LargestAndSmallest.java
@@ -1,24 +1,25 @@
 public class LargestAndSmallest {
 
-	public static void main(String[] args) {
-		/* Define and initialize the array */
-		int[] numbArray = { 5, 48, 3, 33, 7, 67, 98, 17, 46, };
-		int smallest = numbArray[0];
-		int biggest = numbArray[0];
+    public static void main(String[] args) {
+        
+        /* Define and initialize the array */
+        int[] numbArray = { 5, 48, 3, 33, 7, 67, 98, 17, 46, };
+        int smallest = numbArray[0];
+        int biggest = numbArray[0];
 
-		/* Iterate over the array */
-		for (int i = 1; i < numbArray.length; i++) {
+        /* Iterate over the array */
+        for (int i = 1; i < numbArray.length; i++) {
 
-			if (numbArray[i] > biggest) {
-				biggest = numbArray[i];
-			} else if (numbArray[i] < smallest) {
-				smallest = numbArray[i];
-			}
+            if (numbArray[i] > biggest) {
+                biggest = numbArray[i];
+            } else if (numbArray[i] < smallest) {
+                smallest = numbArray[i];
+            }
 
-		}
-		System.out.println("Largest Number is : " + biggest);
-		System.out.println("Smallest Number is : " + smallest);
+        }
+        System.out.println("Largest Number is : " + biggest);
+        System.out.println("Smallest Number is : " + smallest);
 
-	}
+    }
 
 }
