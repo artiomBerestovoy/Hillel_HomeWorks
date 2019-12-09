a1f49c77e459d77df9f9bbc0b23c64ba1d7a14f2
artiomBerestovoy <berstvjj@gmail.com>
2019-12-09 19:17:50 +0200

    modiffied files

--- HW4/src/CarProcessor.java
+++ HW4/src/CarProcessor.java
@@ -1,41 +1,41 @@
 
 class CarProcessor {
 
-	void sortBrand(Car[] cars, String make) {
+    void sortBrand(Car[] cars, String make) {
 
-		for (Car c : cars) {
+        for (Car c : cars) {
 
-			if (c.carMake.equals(make)) {
+            if (c.carMake.equals(make)) {
 
-				System.out.println(c.carId + c.carMake + c.carModel + c.yearOfIssue + c.carColor + c.carCost
-						+ c.registrationNumber);
-			}
-		}
-	}
+                System.out.println(c.carId + c.carMake + c.carModel + c.yearOfIssue + c.carColor + c.carCost
+                        + c.registrationNumber);
+            }
+        }
+    }
 
-	void oldestCar(Car[] cars, String make, int age) {
+    void oldestCar(Car[] cars, String make, int age) {
 
-		for (Car c : cars) {
+        for (Car c : cars) {
 
-			if ((c.carMake.equals(make)) & ((2019 - c.yearOfIssue) < age)) {
+            if ((c.carMake.equals(make)) & ((2019 - c.yearOfIssue) < age)) {
 
-				System.out.println(c.carId + c.carMake + c.carModel + c.yearOfIssue + c.carColor + c.carCost
-						+ c.registrationNumber);
-			}
-		}
-	}
+                System.out.println(c.carId + c.carMake + c.carModel + c.yearOfIssue + c.carColor + c.carCost
+                        + c.registrationNumber);
+            }
+        }
+    }
 
-	void mostExpensive(Car[] cars) {
-		Car pricy = cars[0];
+    void mostExpensive(Car[] cars) {
+        Car pricy = cars[0];
 
-		for (Car c : cars) {
+        for (Car c : cars) {
 
-			if (c.carCost > pricy.carCost) {
-				pricy = c;
-			}
-		}
+            if (c.carCost > pricy.carCost) {
+                pricy = c;
+            }
+        }
 
-		System.out.println(pricy.carId + pricy.carMake + pricy.carModel + pricy.yearOfIssue + pricy.carColor
-				+ pricy.carCost + pricy.registrationNumber);
-	}
+        System.out.println(pricy.carId + pricy.carMake + pricy.carModel + pricy.yearOfIssue + pricy.carColor
+                + pricy.carCost + pricy.registrationNumber);
+    }
 }
