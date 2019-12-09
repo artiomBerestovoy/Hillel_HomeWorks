b78c5c74fef79c7a97f60855b062593098e33c94
artiomBerestovoy <berstvjj@gmail.com>
2019-12-09 19:08:57 +0200

    modiffied files

--- HW4/src/Car.java
+++ HW4/src/Car.java
@@ -1,22 +1,22 @@
 
 class Car {
-    public int       carId;
-    public String    carMake;
-    public String    carModel;
-    public int       yearOfIssue;
-    public String    carColor;
-    public int       carCost;
-    public String    registrationNumber;
-    
-    public Car(int carId, String carMake, String carModel, int yearOfIssue,
-               String carColor, int carCost, String registrationNumber) {
-        this.carId = carId;
-        this.carMake = carMake;
-        this.carModel = carModel;
-        this.yearOfIssue = yearOfIssue;
-        this.carColor = carColor;
-        this.carCost = carCost;
-        this.registrationNumber = registrationNumber;
-    }
+	public int carId;
+	public String carMake;
+	public String carModel;
+	public int yearOfIssue;
+	public String carColor;
+	public int carCost;
+	public String registrationNumber;
+
+	public Car(int carId, String carMake, String carModel, int yearOfIssue, String carColor, int carCost,
+			String registrationNumber) {
+		this.carId = carId;
+		this.carMake = carMake;
+		this.carModel = carModel;
+		this.yearOfIssue = yearOfIssue;
+		this.carColor = carColor;
+		this.carCost = carCost;
+		this.registrationNumber = registrationNumber;
+	}
 
 }
--- HW4/src/CarProcessor.java
+++ HW4/src/CarProcessor.java
@@ -1,45 +1,41 @@
 
 class CarProcessor {
-	
-    void sortBrand(Car[] cars, String make) {
-        
-        for (Car c : cars) {
-        
-            if (c.carMake.equals(make)) {
-    			
-                System.out.println(c.carId + c.carMake + c.carModel
-                        + c.yearOfIssue + c.carColor + c.carCost
-                        + c.registrationNumber);
-            }
-        }
-    }
-    
-    void oldestCar(Car[] cars, String make, int age) {
-        
-        for (Car c : cars) {
-        	
-            if ((c.carMake.equals(make))
-                    & ((2019 - c.yearOfIssue) < age)) {
-        		
-                System.out.println(c.carId + c.carMake + c.carModel
-                        + c.yearOfIssue + c.carColor + c.carCost
-                        + c.registrationNumber);
-            }
-        }
-    }
-    
-    void mostExpensive(Car[] cars) {
-        Car pricy = cars[0];
-        
-        for (Car c : cars) { 
-            
-            if (c.carCost > pricy.carCost) {
-                pricy = c;
-            }
-        }
-        
-        System.out.println(pricy.carId + pricy.carMake + pricy.carModel
-                + pricy.yearOfIssue + pricy.carColor + pricy.carCost
-                + pricy.registrationNumber);
-    }
+
+	void sortBrand(Car[] cars, String make) {
+
+		for (Car c : cars) {
+
+			if (c.carMake.equals(make)) {
+
+				System.out.println(c.carId + c.carMake + c.carModel + c.yearOfIssue + c.carColor + c.carCost
+						+ c.registrationNumber);
+			}
+		}
+	}
+
+	void oldestCar(Car[] cars, String make, int age) {
+
+		for (Car c : cars) {
+
+			if ((c.carMake.equals(make)) & ((2019 - c.yearOfIssue) < age)) {
+
+				System.out.println(c.carId + c.carMake + c.carModel + c.yearOfIssue + c.carColor + c.carCost
+						+ c.registrationNumber);
+			}
+		}
+	}
+
+	void mostExpensive(Car[] cars) {
+		Car pricy = cars[0];
+
+		for (Car c : cars) {
+
+			if (c.carCost > pricy.carCost) {
+				pricy = c;
+			}
+		}
+
+		System.out.println(pricy.carId + pricy.carMake + pricy.carModel + pricy.yearOfIssue + pricy.carColor
+				+ pricy.carCost + pricy.registrationNumber);
+	}
 }
