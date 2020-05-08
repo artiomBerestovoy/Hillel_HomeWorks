547d7e88d36deb8c0bd833b36c883a6cca649828
artiomBerestovoy <berstvjj@gmail.com>
2020-05-08 13:13:45 +0300

    add dev

--- HW1/ConsoleArgs.java
+++ HW1/ConsoleArgs.java
@@ -1,6 +1,6 @@
 public class ConsoleArgs{
     public static void main(String[] args) {
- 
+dev
         for (int i = args.length; i > 0; i--) {
             System.out.print(args[i - 1] + " ");
         }
