7b399c1e04edff1083b116287a8545729fba9ec9
artiomBerestovoy <berstvjj@gmail.com>
2020-05-08 12:59:53 +0300

    modified Consol Args

--- HW1/ConsoleArgs.java
+++ HW1/ConsoleArgs.java
@@ -1,5 +1,5 @@
 public class ConsoleArgs{
-    public static void main(String[] args){
+    public static void main(String[] args) {
  
         for (int i = args.length; i > 0; i--) {
             System.out.print(args[i - 1] + " ");
